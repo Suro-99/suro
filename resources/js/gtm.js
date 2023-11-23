edc14226d7886331a50436b55674de10c24ab9e9
Suro <55480739+Suro-99@users.noreply.github.com>
2023-11-23 15:31:58 +0900

    updates

--- resources/js/gtm.js
+++ resources/js/gtm.js
@@ -39,8 +39,6 @@ function getFullDate(number, length) {
 }
 
 
-
-
 var dataLayer = dataLayer || [];
 var pushData = {
     deviceChannel : getDeviceChannel()
@@ -53,3 +51,42 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5XVB336S');
+
+
+var item = [];
+var aa = 0;
+for(var i = 0;i < 10; i++){
+    aa += (i + 1);
+    var prdObj = {
+        item_name : 'prd_name_' + (i+1) + "_" + getDate(),
+        item_id : 'prd_Id_' + (i+1) + "_" + getDate(),
+        item_brand : 'prd_br_' + (i+1) + "_" + getDate(),
+        item_category : 'prd_cate_1' + (i+1) + "_" + getDate(),
+        item_category2 : 'prd_cate_2' + (i+1) + "_" + getDate(),
+        item_category3 : 'prd_cate_3' + (i+1) + "_" + getDate(),
+        item_category4 : 'prd_cate_4' + (i+1) + "_" + getDate(),
+        item_variant : "prd_option_" + (i+1) + "_" + getDate(),
+        affiliation : "prd_affli_" + (i+1) + "_" + getDate(),
+        coupon : "prd_coupon_" + (i+1) + "_" + getDate(),
+        location_id: "prd_lo_id_" + (i+1) + "_" + getDate(),
+        currency : "KRW",
+        quantity : Number(i+1),
+        price : Number((i+1) * 1000),
+        discount : Number((i+1) * 100),
+    }
+    item.push(prdObj);
+}
+
+
+var ecomObj = {
+  affiliation : "tr_af_" + getDate(),
+  coupon : "tr_co_" + getDate(),
+  currency : "KRW",
+  items : item,
+  shipping : "2500",
+  tax : "0",
+  transaction_id : "tr_Id_" + getDate(),
+  value : "52000"
+}  
+
+ecommercelogEvent("purchase", ecomObj);
